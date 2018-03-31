3d317fa3b99538c7a391d2a9f2e893451d795184
Chad Palsulich <cpalsulich@gmail.com>
2018-03-31 14:25:33 -0700

    finished chapter 12

--- ch12/unfold.hs
+++ ch12/unfold.hs
@@ -1,3 +1,8 @@
+data BinaryTree a =
+  Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
 myIterate :: (a -> a) -> a -> [a]
 myIterate f a = [(f a)] ++ (myIterate f a)
 
@@ -7,3 +12,19 @@ myUnfoldr f b = (fst output) ++ myUnfoldr f (snd output)
         getVals Nothing b = ([], b)
         getVals (Just (a, b)) _ = ([a], b)
         output = getVals (f b) b
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f x = myUnfoldr (\a -> Just (a, f a)) x
+
+unfold :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfold f a = constructTree (f a)
+  where constructTree Nothing = Leaf
+        constructTree (Just (left, root, right))
+          = Node (unfold f left) root (unfold f right)
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfold (func n) 0 
+ where func :: Integer -> Integer -> Maybe (Integer, Integer, Integer)
+       func max x
+         | x == max = Nothing
+         | otherwise = Just (x + 1, x, x + 1)
